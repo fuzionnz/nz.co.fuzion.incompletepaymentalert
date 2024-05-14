b1fe0b88459526c4f1ff4db7ce4b8540b63d11d5
Jitendra Purohit <purohitjitend@gmail.com>
2024-05-14 16:25:08 +0530

    civix upgrade

--- incompletepaymentalert.php
+++ incompletepaymentalert.php
@@ -13,39 +13,12 @@ function incompletepaymentalert_civicrm_config(&$config) {
 }
 
 /**
- * Implements hook_civicrm_xmlMenu().
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_xmlMenu
- */
-function incompletepaymentalert_civicrm_xmlMenu(&$files) {
-  _incompletepaymentalert_civix_civicrm_xmlMenu($files);
-}
-
-/**
  * Implements hook_civicrm_install().
  *
  * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_install
  */
 function incompletepaymentalert_civicrm_install() {
   _incompletepaymentalert_civix_civicrm_install();
-}
-
-/**
- * Implements hook_civicrm_postInstall().
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_postInstall
- */
-function incompletepaymentalert_civicrm_postInstall() {
-  _incompletepaymentalert_civix_civicrm_postInstall();
-}
-
-/**
- * Implements hook_civicrm_uninstall().
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_uninstall
- */
-function incompletepaymentalert_civicrm_uninstall() {
-  _incompletepaymentalert_civix_civicrm_uninstall();
 }
 
 /**
@@ -57,83 +30,6 @@ function incompletepaymentalert_civicrm_enable() {
   _incompletepaymentalert_civix_civicrm_enable();
 }
 
-/**
- * Implements hook_civicrm_disable().
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_disable
- */
-function incompletepaymentalert_civicrm_disable() {
-  _incompletepaymentalert_civix_civicrm_disable();
-}
-
-/**
- * Implements hook_civicrm_upgrade().
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_upgrade
- */
-function incompletepaymentalert_civicrm_upgrade($op, CRM_Queue_Queue $queue = NULL) {
-  return _incompletepaymentalert_civix_civicrm_upgrade($op, $queue);
-}
-
-/**
- * Implements hook_civicrm_managed().
- *
- * Generate a list of entities to create/deactivate/delete when this module
- * is installed, disabled, uninstalled.
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_managed
- */
-function incompletepaymentalert_civicrm_managed(&$entities) {
-  _incompletepaymentalert_civix_civicrm_managed($entities);
-}
-
-/**
- * Implements hook_civicrm_caseTypes().
- *
- * Generate a list of case-types.
- *
- * Note: This hook only runs in CiviCRM 4.4+.
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_caseTypes
- */
-function incompletepaymentalert_civicrm_caseTypes(&$caseTypes) {
-  _incompletepaymentalert_civix_civicrm_caseTypes($caseTypes);
-}
-
-/**
- * Implements hook_civicrm_angularModules().
- *
- * Generate a list of Angular modules.
- *
- * Note: This hook only runs in CiviCRM 4.5+. It may
- * use features only available in v4.6+.
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_angularModules
- */
-function incompletepaymentalert_civicrm_angularModules(&$angularModules) {
-  _incompletepaymentalert_civix_civicrm_angularModules($angularModules);
-}
-
-/**
- * Implements hook_civicrm_alterSettingsFolders().
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_alterSettingsFolders
- */
-function incompletepaymentalert_civicrm_alterSettingsFolders(&$metaDataFolders = NULL) {
-  _incompletepaymentalert_civix_civicrm_alterSettingsFolders($metaDataFolders);
-}
-
-/**
- * Implements hook_civicrm_entityTypes().
- *
- * Declare entity types provided by this module.
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_entityTypes
- */
-function incompletepaymentalert_civicrm_entityTypes(&$entityTypes) {
-  _incompletepaymentalert_civix_civicrm_entityTypes($entityTypes);
-}
-
 // --- Functions below this ship commented out. Uncomment as required. ---
 
 /**
@@ -141,9 +37,8 @@ function incompletepaymentalert_civicrm_entityTypes(&$entityTypes) {
  *
  * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_preProcess
  *
-function incompletepaymentalert_civicrm_preProcess($formName, &$form) {
 
-} // */
+ // */
 
 /**
  * Implements hook_civicrm_navigationMenu().
